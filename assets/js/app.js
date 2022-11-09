f65dae5b636065978ee3c3d894dc58dbca4799c6
mrkamin <108926875+mrkamin@users.noreply.github.com>
2022-11-09 13:36:14 +0500

    Delet all un used code in js fiel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,3 @@
-/* window.onscroll = function () {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    document.getElementById('navbar').classList.add('scrolled');
-  } else {
-    document.getElementById('navbar').classList.remove('scrolled');
-  }
-}; */
-
-/* AOS.init({
-  duration: 800,
-}); */
 
 const presenters = [
   {
